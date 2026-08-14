0ac632acedc4ab6a13f704aa2ed66b6e1b72f09f
hwanju <bbqwwrt@gmail.com>
2026-08-14 09:49:04 +0900

    0813 수업내용

--- ex_0813/src/ex_work/Ex7_work.java
+++ ex_0813/src/ex_work/Ex7_work.java
@@ -13,21 +13,64 @@ public class Ex7_work {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("홀수 : ");
-        int n = sc.nextInt();
+        int size = sc.nextInt();
+        
+        int[][] square = new int[size][size];
 
-        int[][] arr = new int[n][n];
+        int num = 1;
+        int y = 0; // y축(행)
+        int x = 0; // x축(열)
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i] == arr[j] ) {
-                    arr[0][1] += 1;
-                    
-                }
+        x = size / 2;
 
-                System.out.printf("%02d ", arr[i][j]);
+        while ( num <= size * size ) {
+            square[y][x] = num;
+
+            if( num % size == 0){
+                y++;
+            }else{
+                y--;
+                x++;
+            }
+
+            if( y < 0)
+                y = size - 1;
+
+            if(x >= size )
+                x = 0;
+
+            num++;
+        }
+       
+        //배열에 담긴 모든 값을 출력
+
+        for( int i = 0; i < size; i++ ){
+            for( int j = 0; j < size; j++){
+                System.out.printf("%02d ", square[i][j]);
             }
             System.out.println();
         }
+       
+       
+        //System.out.print("홀수 : ");
+        //int n = sc.nextInt();
+
+        //int[][] arr = new int[n][n];
+
+        //int num = 0;
+
+       // for (int i = 0; i < arr.length; i++) {
+            //for (int j = 0; j < arr[i].length; j++) {
+                
+                    
+                    
+                    
+                
+
+               // System.out.printf("%02d ", arr[i][j]);
+           // }
+           // System.out.println();
+        //}
 
     }// main
 }
