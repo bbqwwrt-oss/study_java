550e87311d07935656c1ef335cc9538ea23e748c
hwanju <bbqwwrt@gmail.com>
2026-08-27 16:18:10 +0900

    0825 수업내용

--- ex_0825/src/ex7_homework/Ex1_work.java
+++ ex_0825/src/ex7_homework/Ex1_work.java
@@ -1,5 +1,9 @@
 package ex7_homework;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Ex1_work {
     public static void main(String[] args) {
         
@@ -9,8 +13,28 @@ public class Ex1_work {
         //[5, 3, 5, 1, 5]
         //[2. 3. 1. 1. 3]
         //교집합 : [3, 1]
+        Random rnd = new Random();
 
+        List<Integer> List1 = new ArrayList<Integer>();
+        List<Integer> List2 = new ArrayList<Integer>();
+
+        //교집합을 담기위한 리스트
+        List<Integer> common = new ArrayList<Integer>();
+
+        for( int i = 0; i < 5; i++ ){
+            List1.add( rnd.nextInt(5) + 1 );
+            List2.add( rnd.nextInt(5) + 1 );
+        }        
+        System.out.println( List1 );
+        System.out.println( List2 );
         
+        for( int num : List1 ){
+            if( List2.contains(num) && !common.contains(num) ){
+                common.add(num);
+            }
+        }
+        System.out.print("교집합 : " + common);
+
 
     }//main
 }
--- ex_0825/src/ex8_homework/WorkMain.java
+++ ex_0825/src/ex8_homework/WorkMain.java
@@ -1,11 +1,51 @@
 package ex8_homework;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class WorkMain {
     public static void main(String[] args) {
         
         //3초안에 타자를 치지않으면 랜덤으로 단어가 하나 씩 추가되며.
 
         String[] data = {"apple", "banana", " orange", "grape", "strawberry", "pineapple"};
+        List<String> arr = new ArrayList<String>();
+
+        WorkSub ws = new WorkSub(arr, data);
+        ws.setDaemon(true);
+        ws.start();
+
+        Scanner sc = new Scanner(System.in);
+
+        // try{
+        //     Thread.sleep(100);
+        // }catch(Exception e){
+
+        // }
+
+        while (true) {
+            
+            if( arr.size() == 0 ){
+                System.out.println("클리어!!!");
+                break;
+            }
+
+            System.out.println(arr);
+            System.out.print(">> ");
+            String in = sc.next();
+
+            //사용자가 입력한 단어와 arr에 담긴값을 비교
+            for( int i = 0; i < arr.size(); i++ ){
+
+                if( in.equals(arr.get(i)) ){
+                    arr.remove(i);
+                    break;
+                }
+
+            }
+
+        }
 
     }//main
 }
